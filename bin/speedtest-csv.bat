2d88775d811a1e5091a1dbc93388391efa1402a9
hb <hb@aroma-project.org>
2014-09-07 15:14:50 -0700

    BUG FIX: Batch script failed to locate bash script

--- bin/speedtest-csv.bat
+++ bin/speedtest-csv.bat
@@ -3,9 +3,5 @@
 @rem # Windows Batch script for launching speedtest-csv via
 @rem # bash, cf. MinGW [http://www.mingw.org/]
 @rem ##################################################################
-
-@rem # Get the directory of this script
-for /f %%i in ("%0") do set PWD=%%~dpi
-
-@rem # Call the bash script in the same directory
-bash %PWD%\speedtest-csv %*
+@rem # Call the bash script in the same directory as this script
+bash %~dp0\speedtest-csv %*
