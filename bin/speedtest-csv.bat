d61c32cc76485002bdab7952cd48178984510555
hb <hb@aroma-project.org>
2016-04-04 09:08:09 -0700

    BUG FIX: speedtest-csv.bat did not look for speedtest-csv in the same directory as itself.

--- bin/speedtest-csv.bat
+++ bin/speedtest-csv.bat
@@ -3,12 +3,12 @@
 @rem # Windows Batch script for launching speedtest-csv via
 @rem # bash, cf. MinGW [http://www.mingw.org/]
 @rem ##################################################################
-@rem # Temporarily add Batch script directory to the front of PATH,
+@rem # Temporarily add batch script directory to the front of PATH,
 @rem # before calling bash script. This avoid having to have the
 @rem # bash script on the search PATH.
 set PATH_ORG=%PATH%
-set PATH_TMP=%~dp0;%PATH%
-rem set PATH=%PATH_TMP%
+set PATH_BIN=%~dp0
+set PATH_TMP=%PATH_BIN%;%PATH%
+set PATH=%PATH_TMP%
 bash --login -c "speedtest-csv %*"
 set PATH=%PATH_ORG%
-
